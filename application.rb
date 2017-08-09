9f77ea80a480bda6b3652b9a9a32943bd0457714
biancavlz <bianca@dawandamail.com>
2017-08-09 17:17:28 +0200

    basic configuration

---
+++ application.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'bundler'
+
+Bundler.require
+require 'review'
+
+DataMapper.setup(:default, 'sqlite::memory:')
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+
+get '/' do
+	"hello" 
+end
