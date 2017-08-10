86bfcd3a3f63f38959352030b8e2cc75e7eb719c
biancavlz <bianca@dawandamail.com>
2017-08-10 14:27:27 +0200

    Adds GET action

--- application.rb
+++ application.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'bundler'
 
 Bundler.require
@@ -9,12 +10,23 @@ DataMapper.finalize
 DataMapper.auto_migrate!
 
 
-get '/' do
-	"hello" 
-end
+  get '/' do
+  	"hello" 
+  end
 
-get '/reviews' do
-	content_type :json
-	reviews = Review.all
-	reviews.to_json
-end
+  get '/reviews' do
+   content_type :json
+
+   reviews = Review.all
+   reviews.to_json
+  end
+
+ post '/reviews' do
+  review = Review.new params[:review]
+  if review.save
+   status 201
+  else
+    status 500
+      json review.errors.full_messages
+  end
+ end
