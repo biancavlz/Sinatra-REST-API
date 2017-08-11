e0c1233d4c2ff90f7ae6b685137f02c74685a3c8
Bianca <ing_biank@hotmail.com>
2017-08-11 12:45:02 +0200

    Adds PUT action

--- application.rb
+++ application.rb
@@ -10,23 +10,36 @@ DataMapper.finalize
 DataMapper.auto_migrate!
 
 
-  get '/' do
-  	"hello" 
-  end
+get '/' do
+  "hello"
+end
 
-  get '/reviews' do
-   content_type :json
+get '/reviews' do
+  content_type :json
 
-   reviews = Review.all
-   reviews.to_json
-  end
+  reviews = Review.all
+  reviews.to_json
+end
 
- post '/reviews' do
+post '/reviews' do
   review = Review.new params[:review]
+
   if review.save
-   status 201
+    status 201
   else
     status 500
-      json review.errors.full_messages
+    json review.errors.full_messages
   end
- end
+end
+
+put '/reviews/:id' do
+  review = Review.get.params[:id]
+
+  if review.update params[:review]
+    status 200
+    json "Review was updated successfully."
+  else
+    status 500
+    json review.errors.full_messages
+  end
+end
